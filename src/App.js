05ec4943da9e51115b99234d1a7434aa5f972cff
rodrigo <rodrigorooliveira@gmail.com>
2024-09-04 11:52:22 -0300

    primeiro commit parte 3

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import { Component } from 'react';
+
+import { Posts } from './components/Posts';
+import { loadPosts } from './utils/load-posts';
+
+class App extends Component {
+  state = {
+
+    posts: []
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+
+  }
+
+  
+  loadPosts = async () => {
+    
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
+  }
+
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <section className='container'>
+         <Posts posts={posts}/>
+      </section>
+    );
+  }
+
+}
+export default App;
